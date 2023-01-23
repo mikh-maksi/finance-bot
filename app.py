3e4d3fad007a873eb4838df4ebe6f9ce7d31817c
Mikhail <newlevelcenter@gmail.com>
2023-01-23 14:50:27 +0200

    bot12

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ async def echo(update, context):
 
     await update.message.reply_text(string_out)
 
-application = Application.builder().token("5593169409:AAEgZq9VPtQd-_S_ki3SQm7GO0KNvXcZItc").build()
+application = Application.builder().token("2034824924:AAFc0q0PYPezeZ6G5kE10uBWhWSurKks-8A").build()
 
 application.add_handler(CommandHandler('income', income))
 application.add_handler(MessageHandler(filters.TEXT, echo))
